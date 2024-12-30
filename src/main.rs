33437f48e5126432e503a6902b826127f2f2e159
Tomás Paradelo <tomas.paradelo@lambdaclass.com>
2024-12-30 17:35:11 -0300

    refactor main

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use lc3::vm::VM;
 use std::{env, io, os::fd::AsRawFd};
 use termios::{Termios, ECHO, ICANON, TCSANOW};
 
-fn main() -> Result<(), lc3::vm::VMError> {
+fn set_termios() -> Termios {
     // Get the current terminal settings
     let stdin = io::stdin();
     let mut termios = if let Ok(termios) = Termios::from_fd(stdin.lock().as_raw_fd()) {
@@ -23,25 +23,42 @@ fn main() -> Result<(), lc3::vm::VMError> {
         std::process::exit(1);
     });
 
+    original_termios
+}
+
+fn restore_termios(original_termios: Termios) {
+    let stdin = io::stdin();
+    termios::tcsetattr(stdin.lock().as_raw_fd(), TCSANOW, &original_termios).unwrap_or_else(|e| {
+        println!("Unable to restore configuration, error {e}");
+        std::process::exit(1);
+    });
+}
+
+fn main() -> Result<(), lc3::vm::VMError> {
+    // config terminal
+    let original_termios = set_termios();
+
+    // collect file to execute
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
         println!("make run FILEPATH=<path/to/file>");
         std::process::exit(1);
     }
-    let mut vm = VM::new();
     let Some(file_path) = args.get(1) else {
         std::process::exit(1);
     };
+
+    // create VM
+    let mut vm = VM::new();
+    // Load program
     vm.read_image(file_path)?;
+    // run program
     if let Err(e) = vm.run() {
         println!("{e:?}");
         std::process::exit(1);
     };
 
-    termios::tcsetattr(stdin.lock().as_raw_fd(), TCSANOW, &original_termios).unwrap_or_else(|e| {
-        println!("Unable to restore configuration, error {e}");
-        std::process::exit(1);
-    });
-
+    // restore terminal
+    restore_termios(original_termios);
     Ok(())
 }
